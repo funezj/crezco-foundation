4375041c1ab0639843d4aca4476bab3cf2a2bfa4
Nerm <nerm.animator@gmail.com>
2023-05-01 23:45:45 -0700

    Add request to contact form

--- src/contact-us/contact-us.module.ts
+++ src/contact-us/contact-us.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { SharedModule } from 'src/shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ContactUsComponent],
-  imports: [SharedModule, CommonModule],
+  imports: [ReactiveFormsModule, SharedModule, CommonModule],
   exports: [ContactUsComponent],
 })
 export class ContactUsModule {}
--- src/contact-us/contact-us/contact-us.component.ts
+++ src/contact-us/contact-us/contact-us.component.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.scss'],
 })
-export class ContactUsComponent {}
+export class ContactUsComponent {
+  contactForm = this.fb.group({
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    inputName: ['', Validators.required],
+    //lastName: [''],
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    inputEmail: ['', Validators.email],
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    amount: ['', Validators.min(20)],
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    messageTextArea: ['', Validators.required],
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    checkForm: ['', Validators.required],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  onSubmited() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.contactForm.value);
+  }
+}
